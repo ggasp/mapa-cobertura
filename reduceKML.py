6534d057ded9db6aa2e846ff1195394756097635
Gabriel Gasparolo <gabriel@gasparolo.com>
2021-11-02 11:31:25 -0300

    Automatizacion de la reducción de tamaño del KML

--- reduceKML.py
+++ reduceKML.py
@@ -1,21 +1,34 @@
 ################################################################################
-## reduceKML: 
+## reduceKML.py: 
 ##           Reduce el tamaño de los archivos KML al bajar la precisión a 
 ## metros (precisión 5) en lugar de la precisión a nanometros con que llegan
 ## los archivos. Además se elimina el componente de altura. 
+##
+## Uso: python3 reduceKML <kml-file> <tecnologia>
+##
+## Cualquier duda me escribes: gabriel at gasparolo.com
 ################################################################################
  
 import re,os,sys
 
 kml_loc=sys.argv[1]
+tecnologia=sys.argv[2]
 f=open(kml_loc)
 lines=f.readlines()
 f.close()
 
+style_replace= {
+    '2G': '\g<1><Style><LineStyle><color>FFA8A8A8</color></LineStyle><PolyStyle><fill>0</fill></PolyStyle></Style>\g<2>',
+    '3G': '\g<1><Style><LineStyle><color>FFEB973E</color></LineStyle><PolyStyle><fill>0</fill></PolyStyle></Style>\g<2>',
+    '4G': '\g<1><Style><LineStyle><color>FF3636FA</color></LineStyle><PolyStyle><fill>0</fill></PolyStyle></Style>\g<2>',
+    '5G': '\g<1><Style><LineStyle><color>FF36B9FA</color></LineStyle><PolyStyle><fill>0</fill></PolyStyle></Style>\g<2>'
+}
+
 all_text_0=''.join(lines)
 all_text_0=' '.join(all_text_0.split())
-all_text_1=re.sub('<description>.*?</description>','<description></description>',all_text_0)
-all_text_1=re.sub('<styleUrl>.*?</styleUrl>','',all_text_1)
+all_text_1=re.sub('<description>.*?</description>', '<description></description>', all_text_0)
+all_text_1=re.sub('<styleUrl>.*?</styleUrl>', '', all_text_1)
+all_text_1=re.sub('(<Placemark .*?>).*?(<ExtendedData>)', style_replace[tecnologia], all_text_1)
 all_text_2=all_text_1
 
 precision=5 # reducir la cantidad de decimales con una precisión de metros (5) o centímetros (6)
@@ -25,6 +38,7 @@ x1=x2=0
 for i in re.findall('<coordinates>(.*?)</coordinates>',all_text_1):
     coord=i.split(' ')
     fixed_part=''
+    cant_p= 0
     for c in coord:
         if( c ): 
             point=c.split(',')
@@ -33,13 +47,12 @@ for i in re.findall('<coordinates>(.*?)</coordinates>',all_text_1):
             # p3= altura no es necesario
             dist = (( float(p1) - x1)**2 + ( float(p2) - x2)**2)**0.5
             if( dist > d ):
+                cant_p+=1
                 fixed_part+=' ' + p1 + ',' + p2
                 x1= float(p1)
                 x2= float(p2)
-    all_text_2=re.sub(i,fixed_part,all_text_2,1)
+    if (cant_p > 3): all_text_2=re.sub(i,fixed_part,all_text_2,1) # no me gustan los triangulos
 
-new_kml=list(os.path.splitext(kml_loc))
-new_kml[0]+='_reduced'
-f=open(''.join(new_kml),'wt')
+f=open(kml_loc,'wt')
 f.write(all_text_2)
 f.close()
---
+++ reduceKML.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+################################################################################
+## reduceKML.sh: 
+##               Recorre el directorio ./kml y por cada archivo .kml, ejecuta
+## el script <reduceKML.py> 
+##
+## Cualquier duda me escribes: gabriel at gasparolo.com
+################################################################################
+
+echo Procesando para Tecnologia 2G
+for f in $(ls kml/2G/*.kml); do
+    echo Procesando archivo $f 
+    cp "$f" "${f%.*}-original.kml"
+    python3 reduceKML.py $f 2G
+done
+
+echo Procesando para Tecnologia 3G
+for f in $(ls kml/3G/*.kml); do
+    echo Procesando archivo $f 
+    cp "$f" "${f%.*}-original.kml"
+    python3 reduceKML.py $f 3G
+done
+
+echo Procesando para Tecnologia 4G
+for f in $(ls kml/4G/*.kml); do
+    echo Procesando archivo $f 
+    cp "$f" "${f%.*}-original.kml"
+    python3 reduceKML.py $f 4G
+done
+
+echo Procesando para Tecnologia 5G
+for f in $(ls kml/5G/*.kml); do
+    echo Procesando archivo $f
+    cp "$f" "${f%.*}-original.kml"
+    python3 reduceKML.py $f 5G
+done
+
